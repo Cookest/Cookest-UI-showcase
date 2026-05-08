1450e4f7c4a9190510fe0747b2b1ea74ad03ba73
Tiago Oliveira <tiagoco2008@gmail.com>
2026-05-08 15:30:16 +0100

    (fix): fixed turbopack build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@cookest/ui"],
-  webpack: (config) => {
-    config.resolve.symlinks = true;
-    return config;
+  turbopack: {
+    root: "../",
   },
 };
 
